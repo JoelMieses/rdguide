a4ab133de2b0afbba424baa52034ab6a3e8963f9
ric7x <ricardorosario89@gmail.com>
2020-08-17 23:06:00 -0400

    bloc,provider,model de rserva

---
+++ lib/bloc/reserva_bloc.dart
@@ -0,0 +1,68 @@
+
+
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:rdguide/bloc/bloc.dart';
+import 'package:rdguide/models/reserva.dart';
+import 'package:rdguide/providers/reserva_provider.dart';
+
+class ReservaBloc extends Bloc{
+
+
+
+
+  final StreamController _reservaController = StreamController<Reserva>.broadcast();
+  Function(Reserva) get _reservaSink => _reservaController.sink.add;
+  Stream<Reserva> get reservaStream => _reservaController.stream;
+
+
+  final StreamController _loadingController = StreamController<bool>.broadcast();
+  Function(bool) get _loadingSink => _loadingController.sink.add;
+  Stream<bool> get loadingStream => _loadingController.stream;
+
+  final StreamController _errorController = StreamController<String>.broadcast();
+  Function(String) get _errorSink => _errorController.sink.add;
+  Stream<String> get errorStream => _errorController.stream;
+
+
+
+  void enviar(){
+    _loadingSink(true);
+  }
+
+  void reserva({@required Reserva reserva,@required BuildContext context})async{
+
+
+    await reservaProvider.postReserva(reserva)
+        .then((result)async{
+
+      _loadingSink(false);
+      if(result != null){
+        if(result.idreserva != "0" ){
+         // Navigator.pop(context);
+        }
+      }
+      _reservaSink(result);
+
+    })
+        .catchError((error,stackTrace){
+      _loadingSink(false);
+      _errorSink(error['mensaje']);
+
+    });
+
+  }
+
+  @override
+  void dispose() {
+    _reservaController.close();
+    _loadingController.close();
+    _errorController.close();
+  }
+
+
+
+
+}
--- lib/pages/realizar_reserva.dart
+++ lib/pages/realizar_reserva.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:rdguide/bloc/bloc_provider.dart';
+import 'package:rdguide/bloc/reserva_bloc.dart';
+import 'package:rdguide/models/reserva.dart';
+import 'package:rdguide/models/usuario.dart';
+import 'package:intl/intl.dart';
 
 
 class RealizarReservaPage extends StatefulWidget {
@@ -6,158 +11,185 @@ class RealizarReservaPage extends StatefulWidget {
   _RealizarReservaPageState createState() => _RealizarReservaPageState();
 }
 
+Reserva reserva = new Reserva();
+String claveError;
+
+
+final _formKey = GlobalKey<FormState>();
 
 class _RealizarReservaPageState extends State<RealizarReservaPage> {
 
   String _fecha = '';
 
- 
-  TextEditingController _inputFieldDateController = new TextEditingController();
+  final _idreservaController = new TextEditingController();
+  final _idclienteController = new TextEditingController();
+  final _idareaController = new TextEditingController();
+  final _cantidadController = new TextEditingController();
+  final _personaController = new TextEditingController();
+  //final _desdeController = new TextEditingController();
+  //final _hastaController = new TextEditingController();
+  final _comentarioontroller = new TextEditingController();
+  final bloc = ReservaBloc();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Realizar Reserva'),
+    return BlocProvider(
+      bloc: bloc,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Realizar reserva'),
+          backgroundColor: Colors.green,
+
+        ),
+        body: _form()
       ),
-      body: Center(
-        child: ListView(
-          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 20.0,),
+    );
+  }
+
+Widget _form(){
+    return Form(
+      key: _formKey,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            //aqui se crean los metodos
-            _nombreInput(),
-            SizedBox(height:20.0),
-            _telefonoInput(),
-            SizedBox(height:20.0),
-            _creaFecha(context),
-            SizedBox(height:20.0),
-            _textogrande(),
-            SizedBox(height:20.0),
-            _pagar(),
+            _textInput(controller: _idreservaController,hint: "idreserva",icon: Icons.account_circle),
+            Divider(),
+            _textInput(controller: _idclienteController,hint: "cliente id",icon: Icons.account_circle),
+            Divider(),
+            _textInput(controller: _idareaController,hint: "area",icon: Icons.account_circle),
+            Divider(),
+            _textInput(controller: _cantidadController,hint: "cantidad hab o mesas",icon: Icons.account_circle),
+            Divider(),
+            _textInput(controller: _personaController,hint: "cant personas",icon: Icons.account_circle),
+            Divider(),
+            _textInput(controller: _comentarioontroller,hint: "comentario",icon: Icons.account_circle),
+            //_sexo(),            
+            Divider(),
+            //_creaFecha(context),
+         
+            _registrar(),
           ],
         ),
       ),
-      );
-  }
+    );
+}
 
-  
-  //este widget es el correspondiente al campo nombre
-  Widget _nombreInput() {
 
-    return TextField(
-      //autofocus: true,
+
+  //este widget es el correspondiente al nombre
+  Widget _textInput({controller: TextEditingController, icon: IconData,hint:String}) {
+    return TextFormField(
+      controller: controller,
+      validator: (value){
+        if(value.isEmpty){
+          return "Este campo no puede estar vacio";
+        }return null;
+      },
       textCapitalization: TextCapitalization.sentences,
       decoration: InputDecoration(
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20.0)
+            borderRadius: BorderRadius.circular(20.0)
         ),
-        hintText: 'Nombre Completo',
-        //labelText: 'Nombre Completo',
-        suffixIcon: Icon(Icons.account_circle),
-        //icon: Icon(Icons.account_circle)
-        
+        hintText: hint,
+        prefixIcon: Icon(
+          icon,
+          color: Colors.grey,
+        ),
       ),
     );
 
   }
 
-  //este widget es el correspondiente al campo telefono
-  Widget _telefonoInput() {
 
-    return TextField(
-      //autofocus: true,
-      textCapitalization: TextCapitalization.sentences,
-      decoration: InputDecoration(
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20.0)
-        ),
-        hintText: 'Teléfono',
-        //labelText: 'Teléfono',
-        suffixIcon: Icon(Icons.phone),
-        //icon: Icon(Icons.phone_iphone)
-        
-      ),
-    );
 
-  }
+  //este widget es el correspondiente al campo fecha de nacimiento
+  /*_creaFecha( BuildContext context) {
 
-  //este widget es el correspondiente al campo fecha
-  _creaFecha( BuildContext context) {
+    //final format = DateFormat("yyyy-MM-dd");
     return TextField(
       enableInteractiveSelection: false,
       controller: _inputFieldDateController,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
             borderRadius: BorderRadius.circular(20.0)
-          ),
-          labelText: 'Fecha de Reservación',
-          suffixIcon: Icon(Icons.calendar_today),
-         // icon: Icon(Icons.calendar_today)
-          
         ),
-        //aqui lo que hacemos es que cuando se haga click en el boton llame al metodo _selectDate para que 
-        //el datepicker abra el selector de fechas
+        hintText: 'Fecha de nacimiento',
+        labelText: 'Fecha de nacimiento',
+        prefixIcon: Icon(
+          Icons.calendar_today,
+          color: Colors.grey,
+        ),
+
+      ),
+      //aqui lo que hacemos es que cuando se haga click en el boton llame al metodo _selectDate para que
+      //el datepicker abra el selector de fechas
       onTap: (){
 
         FocusScope.of(context).requestFocus(new FocusNode());
         _selectDate(context);
 
-      } ,  
+      } ,
     );
-  }
-   //este es el metodo que permite la funcionalidad del selector de fechas
-  _selectDate(BuildContext context) async{
+  }*/
+  //este es el metodo que permite la funcionalidad del selector de fechas
+  _selectDate(BuildContext context0) async{
 
-      DateTime picked =  await showDatePicker(
+    DateTime picked =  await showDatePicker(
         context: context,
         initialDate: new DateTime.now(),
-        firstDate: new DateTime(2018),
-        lastDate: new DateTime(2025),
+        firstDate: new DateTime(1940),
+        lastDate: new DateTime.now(),
         locale: Locale('es', 'ES')
-      );
+    );
 
-      if (picked !=null){
-        setState(() {
-          _fecha = picked.toString();
-          _inputFieldDateController.text = _fecha;
-        });
-      }
+    if (picked !=null){
+      setState(() {
+
+        _fecha = "${DateFormat('yyyy-MM-dd').format(picked)}";
+       // _inputFieldDateController.text = _fecha;
+      });
+    }
   }
 
- 
-   //este widget es el correspondiente al campo texto
-  Widget _textogrande(){
-    return TextFormField(
-       textCapitalization: TextCapitalization.sentences,
-      decoration:  InputDecoration(
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(20.0)
-        ),
-        hintText: 'Descripción',
-        suffixIcon: Icon(Icons.message),
-        //labelText: 'Requerimiento Especial',
-        //icon: Icon(Icons.library_books)
+  //este widget es el correspondiente al campo boton registro de usuario
+
+  Widget _registrar(){
+
+    return RaisedButton(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 15.0),
+        child: Text('Reservar'),
       ),
-      maxLines: 3,
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0)
+      ),
+      elevation: 0.0,
+      color: Colors.green,
+      textColor: Colors.white,
+      onPressed: (){
+
+        if(_formKey.currentState.validate()){
+
+         registrar();
+
+          
+
+        }
+      },
     );
   }
+  void registrar(){
+    reserva = Reserva(idreserva:0,idcliente: 1 ,idarea:1, cantidad:2, personas: 3, desde:'2020-09-09',hasta:'2020-09-09',comentario:'esto es un comentario'  );
 
-  Widget _pagar(){
-    return RaisedButton(
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
-                            color: Colors.green,
-                            textColor: Colors.white,
-                            child: Text("Pagar ahora", style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontSize: 18
-                            ),),
-                            padding: const EdgeInsets.symmetric(
-                              vertical: 16.0,
-                              horizontal: 32.0,
-                            ),
-                            onPressed: () {
-                              Navigator.of(context).pushNamed('');},
-                          );
+    print(reserva);
+    bloc.reserva(reserva: reserva, context: context);
   }
   
 }
+
+
+
+
+
+
